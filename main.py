0cb18468547a1454dde8605684a14884d5511438
Luciano <lushobarlett@gmail.com>
2022-02-07 14:22:40 -0300

    inline calculations class

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import json
+from math import pi, sqrt
 import os
+import signal
 
-from calculations import Calculations
 from plot import Plot
 from state import State
 
@@ -46,14 +47,111 @@ def read_frequency_relative_errors(filename):
 
     return relative_errors
 
+def calc_kinetic(params, velocities_list):
+    kinetic = []
+
+    for velocities in velocities_list:
+
+        kinetic_t = 0
+
+        for i in range(params['osc_num']):
+            kinetic_t += velocities[i] ** 2
+
+        kinetic_t *= params['mass'] / 2
+
+        kinetic.append(kinetic_t)
+    
+    return kinetic
+
+def calc_potential(params, displacements_list):
+    potential = []
+
+    for displacements in displacements_list:
+
+        potential_t = 0
+
+        for i in range(params['osc_num'] - 1):
+            potential_t += (displacements[i + 1] - displacements[i]) ** 2
+
+        potential_t *= params['k'] / 2
+
+        potential.append(potential_t)
+    
+    return potential
+
+def calc_E(params, displacements_list, velocities):
+    kinetic = calc_kinetic(params, velocities)
+    potential = calc_potential(params, displacements_list)
+
+    total = []
+    for k, p in zip(kinetic, potential):
+        total.append(k + p)
+
+    return (total, kinetic, potential)
+
+def mean_frequency(params, displacements_list, times):
+    middle_oscilator_displacements = []
+
+    for displacements in displacements_list:
+        middle = int(params['osc_num'] / 2)
+        middle_oscilator_displacements.append(displacements[middle])
+
+    indices, _ = signal.find_peaks(middle_oscilator_displacements)
+
+    period_approximates = []
+
+    for i in range(len(indices) - 1):
+        tmax1 = times[indices[i]]
+        tmax2 = times[indices[i + 1]]
+        period_approximates.append(abs(tmax1 - tmax2))
+
+    mean_period = 0
+
+    for p in period_approximates:
+        mean_period += p
+
+    mean_period /= len(period_approximates)
+
+    if mean_period == 0:
+        return None
+
+    return 2 * pi / mean_period
+
+def calc_wave_vel(params, velocities_list, times):
+    last_oscilator_velocities = []
+
+    for velocities in velocities_list:
+        last_oscilator_velocity = velocities[len(velocities) - 1]
+        last_oscilator_velocities.append(last_oscilator_velocity)
+
+    time_of_movement = 0
+    for t, v in zip(times, velocities):
+        time_of_movement = t
+        if v > 0:
+            break
+
+    spring_amount = params['osc_num'] - 1
+    length_of_rest = params['l_rest'] * spring_amount
+
+    return length_of_rest / time_of_movement
+
+def calc_wave_vel_analitic(params):
+    return sqrt(params['k'] / params['mass']) * params['l_rest']
+
+def relative_error(real, approx):
+    return abs(real - approx) / real
+
 def main():
-    params = params_from_file("input_params.json")
+    params = params_from_file("input_parameters.json")
     params['simulation'] = 1
     params['frame'] = 1
 
     for _ in params['frames_num']:
 
-        calculations = Calculations(params)
+        displacements_list = []
+        velocities_list = []
+        times = []
+
         plot = Plot()
         state = state_from_file("input_initState.json", params)
 
@@ -67,11 +165,9 @@ def main():
             current_accelerations = state.current_accelerations()
             current_time = state.current_time()
 
-            calculations.register_displacements_and_velocities(
-                current_displacements,
-                current_velocities,
-                current_time
-            )
+            displacements_list.append(current_displacements)
+            velocities_list.append(current_velocities)
+            times.append(current_time)
 
             middle = int(params['osc_num'] / 2)
 
@@ -86,10 +182,10 @@ def main():
 
             params['frame'] += 1
 
-        total, kinetic, potential = calculations.calc_E()
+        total, kinetic, potential = calc_E(params, displacements_list, velocities_list)
 
         if params['omega'] != None:
-            omega_approx = calculations.mean_frequency()
+            omega_approx = mean_frequency(params, displacements_list, times)
 
             if omega_approx == None:
                 print(f"T_avg = 0 for dt = {params['dt'][params['simulation'] - 1]}")
@@ -97,23 +193,17 @@ def main():
             write_numbers("frequencies.txt", [
                 params['omega'],
                 omega_approx,
-                Calculations.relative_error(
-                    params['omega'],
-                    omega_approx
-                )
+                relative_error(params['omega'], omega_approx)
             ])
 
         if params['osc_num'] > 100 and params['first_is_open'] and params['last_is_open']:
-            wave_velocity_approx = calculations.calc_wave_vel()
-            wave_velocity_analitic = calculations.calc_wave_vel_analitic()
+            wave_velocity_approx = calc_wave_vel(params, velocities_list, times)
+            wave_velocity_analitic = calc_wave_vel_analitic(params)
 
             write_numbers("v_wave.txt", [
                 wave_velocity_analitic,
                 wave_velocity_approx,
-                Calculations.relative_error(
-                    wave_velocity_analitic,
-                    wave_velocity_approx
-                )
+                relative_error(wave_velocity_analitic, wave_velocity_approx)
             ])
 
         plot.kinematics("kinematics.png")
